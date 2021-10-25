23e2736756e91dfd29bfd156a1259813dbf6f7bf
Chase-Stokes <chasehstokes@gmail.com>
2021-10-25 11:18:43 -0700

    add forms and display the input

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Buiness logic
+// function yourFav(input) {
+//   return yourFavorites ;
+// };
+
+$(document).ready(function(){
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+    const favPerson = $("#aPerson").val();
+    const place = $("#aPlace").val();
+    const movie = $("#aMovie").val();
+    const yourFavorites = [favPerson, place, movie];
+    let second = yourFavorites[1];
+    let first = yourFavorites[0];
+    let third = yourFavorites[2];
+
+    $(".person").text(second);
+    $(".place").text(first);
+    $(".movie").text(third);
+    $(".favList").text(yourFavorites);
+  });
+});
